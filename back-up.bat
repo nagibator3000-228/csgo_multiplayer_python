b4e05e733f38259cc71ddf502202af17b1a580c5
[PUZ]Cklebepand <116970894+nagibator3000-228@users.noreply.github.com>
2023-07-08 14:58:46 +0200

    added path

--- back-up.bat
+++ back-up.bat
@@ -1,5 +1,11 @@
+@REM git status
+@REM git add -u
+@REM git status
+@REM git commit -m "auto save"
+@REM git push
+
 git status
 git add -u
 git status
-git commit -m "auto save"
+git commit -m "added path"
 git push
